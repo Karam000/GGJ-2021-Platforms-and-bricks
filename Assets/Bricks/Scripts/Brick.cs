8be22fdee471931000da6096a8117800206c6499
Nora Muhammad <noramohamedfcis@gmail.com>
2021-01-29 16:27:46 +0200

    Initial bricks rotating around node center with required number

---
+++ Assets/Bricks/Scripts/Brick.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    [SerializeField] Node level;
+    [SerializeField] TextMesh requiredNumberText;
+    [SerializeField] GameObject gemPrefab;
+    [SerializeField] GameObject explodePrefab;
+
+    void Start()
+    {
+        requiredNumberText.text = level.NodeRequiredNumber.ToString();
+    }
+    private void Update()
+    {
+        switch (level.currentState)
+        {
+            case Node.NodeState.Rotate:
+                RotateAroundLevelCenter();
+                break;
+
+            case Node.NodeState.Explode:
+                ExplodeBrick();
+                break;
+        }
+    }
+
+    #region HelperFunctions
+    public void RotateAroundLevelCenter()
+    {
+        transform.RotateAround(level.NodeCenter.position, Vector3.up, level.NodeRotationSpeed * Time.deltaTime);
+    }
+    void ExplodeBrick()
+    {
+        PopupGems();
+        Invoke("Explode", 1f);
+    }
+    void PopupGems()
+    {
+
+    }
+    void Explode()
+    {
+        requiredNumberText.gameObject.SetActive(false);
+
+    }
+    #endregion
+}
---
+++ Assets/Node/Scripts/Node.cs
@@ -0,0 +1,27 @@
+﻿using SO.Events;
+using System;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Serializable]
+    public enum NodeState
+    {
+        Rotate,
+        Explode
+    }
+
+    public int NodeRequiredNumber;
+    public Transform NodeCenter;
+    public float NodeRotationSpeed;
+    public NodeState currentState;
+
+    private void Start()
+    {
+        currentState = NodeState.Rotate;
+    }
+    public void CheckForLevelStateUpdate()
+    {
+
+    }
+}
