64ac1893be25a6c384eaaba8415007c0c00da8d9
Mohamed Karam <mohamedkaram09@gmail.com>
2021-01-29 16:00:09 +0200

    path showing done

---
+++ Assets/Level/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SO.Events;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] EventListenerSO levelStarted;
+    [SerializeField] EventListenerSO levelEnded;
+
+    [SerializeField] PathController PathController;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnLevelStatrted()
+    {
+        PathController.HidePath();
+    }
+
+    public void OnLevelEnded()
+    {
+        PathController.ShowPath();
+    }
+}
---
+++ Assets/Path/Scripts/PathController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathController : MonoBehaviour
+{
+
+    [SerializeField] GameObject pathUnitPrefab;
+
+    List<Transform> pathUnits = new List<Transform>();
+
+    bool firstGizmos = true;
+    bool runMode = false;
+    void Start()
+    {
+        runMode = true;
+        Transform[] temp = this.transform.GetComponentsInChildren<Transform>();
+        for (int i = 1; i < temp.Length; i++)
+        {
+            pathUnits.Add(temp[i]);
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    public void ShowPath()
+    {
+        foreach (Transform pathunit in pathUnits)
+        {
+            Instantiate(pathUnitPrefab, pathunit.position, pathunit.rotation, pathunit);
+        }
+    }
+
+    public void HidePath()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        if (!runMode)
+        {
+            if (firstGizmos)
+            {
+                firstGizmos = false;
+                Transform[] temp = this.transform.GetComponentsInChildren<Transform>();
+                for (int i = 1; i < temp.Length; i++)
+                {
+                    pathUnits.Add(temp[i]);
+                }
+            } 
+        }
+
+        if (pathUnits.Count > 0)
+        {
+            Gizmos.color = Color.green;
+            for (int i = 0; i < pathUnits.Count - 1; i++)
+            {
+                    Gizmos.DrawLine(pathUnits[i].position, pathUnits[i + 1].position);
+            }
+
+            Gizmos.color = Color.red;
+            for (int i = 0; i < pathUnits.Count; i++)
+            {
+                    Gizmos.DrawSphere(pathUnits[i].position,0.05f);
+            }
+        }
+    }
+
+}
