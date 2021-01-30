7cc944323e22334d1eefc11b4f7fea96e593c1d3
Nora Muhammad <noramohamedfcis@gmail.com>
2021-01-30 16:59:01 +0200

    Initial platform state prediction

---
+++ Assets/GameUI/Scripts/PlatformTimer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformTimer : MonoBehaviour
+{
+    [SerializeField] float platformMaxTime;
+    [SerializeField] Image timerFill;
+
+    public void StartTimer()
+    {
+
+    }
+}
--- Assets/Platforms/Scripts/PlatformBehavior.cs
+++ Assets/Platforms/Scripts/PlatformBehavior.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class PlatformBehavior : MonoBehaviour
 {
+    [SerializeField] PlatformTimer timer;
     public int maxNumberOfJumps;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag(GameTags.PlayerTag))
+        {
+            timer.StartTimer();
+        }
+    }
 }
