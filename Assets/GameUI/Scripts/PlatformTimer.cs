eb96a58cbd6026c2503d2436c8ef7d90309eb87d
Nora Muhammad <noramohamedfcis@gmail.com>
2021-01-30 17:55:23 +0200

    Platform timer

--- Assets/GameUI/Scripts/PlatformTimer.cs
+++ Assets/GameUI/Scripts/PlatformTimer.cs
@@ -5,11 +5,34 @@ using UnityEngine.UI;
 
 public class PlatformTimer : MonoBehaviour
 {
-    [SerializeField] float platformMaxTime;
+    [SerializeField] float timerSpeed;
     [SerializeField] Image timerFill;
-
+    Coroutine timerCo;
     public void StartTimer()
     {
+        timerCo = StartCoroutine(StartPlatformTimer());
+    }
+    public void StopTimer()
+    {
+        if(timerCo != null)
+            StopCoroutine(timerCo);
 
+        timerFill.fillAmount = 1;
+    }
+    public void ResumeTimer()
+    {
+
+    }
+    IEnumerator StartPlatformTimer()
+    {
+        yield return new WaitUntil(ReduceTimerFill);
+        StopCoroutine(timerCo);
+    }
+    bool ReduceTimerFill()
+    {
+        Debug.Log(timerSpeed * Time.deltaTime);
+        timerFill.fillAmount -= (timerSpeed * Time.deltaTime);
+
+        return timerFill.fillAmount == 0;
     }
 }
--- Assets/Platforms/Scripts/PlatformBehavior.cs
+++ Assets/Platforms/Scripts/PlatformBehavior.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class PlatformBehavior : MonoBehaviour
 {
     [SerializeField] PlatformTimer timer;
+    [SerializeField] float toleranceDuration;
     public int maxNumberOfJumps;
     public bool isLevelLastPlatform;
-
+    float collisionTime;
+    bool timerStarted;
     private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(GameTags.PlayerTag))
+        {
+            Debug.Log("Enter");
+            collisionTime = Time.time;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
     {
         if(collision.collider.CompareTag(GameTags.PlayerTag))
         {
-            timer.StartTimer();
+            Debug.Log((Time.time - collisionTime));
+            if((Time.time - collisionTime) > toleranceDuration && !timerStarted)
+            {
+                timer.StartTimer();
+                timerStarted = true;
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag(GameTags.PlayerTag))
+        {
+            Debug.Log("Exit");
+            timer.StopTimer();
+            timerStarted = false;
         }
     }
 }
